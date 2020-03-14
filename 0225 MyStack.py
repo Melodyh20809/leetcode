cfe2d254e326c0c01cd3cc57ff272778abf21609
Melody <melodyh20809@gmail.com>
2020-03-14 22:06:46 +0800

    push

---
+++ 0225 MyStack.py
@@ -0,0 +1,46 @@
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.stack=[]
+        
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        self.stack.insert(0,x)
+        
+
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        return self.stack.pop(0)
+
+    def top(self) -> int:
+        """
+        Get the top element.
+        """
+        return self.stack[0]
+        
+
+    def empty(self) -> bool:
+        """
+        Returns whether the stack is empty.
+        """
+        if not self.stack:
+            return True
+        else :
+            return False
+x=MyStack()
+x.push(0)
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
